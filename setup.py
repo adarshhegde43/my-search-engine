b2f9fc9f15d18a417d0a79797687d38176783385
Adarsh Hegde <ch22b048@smail.iitm.ac.in>
2025-03-02 10:01:27 +0530

    Modified setup.py to install correct files

--- setup.py
+++ setup.py
@@ -1,17 +1,16 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
-    name = "microsearch",
-    version = "1.0.0",
-    description = "A small search library.",
+    name="microsearch",
+    version="1.0.0",
+    description="A small search library.",
     long_description=open('README.rst', 'r').read(),
-    py_modules = [
-        'microsearch'
-    ],
-    classifiers = [
+    py_modules=["microsearch"],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Everyone',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Topic :: Internet :: WWW/HTTP :: Indexing/Search'
-    ],
+    ]
 )
